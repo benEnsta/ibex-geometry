9b72771dfca2f9d77b7db223347007efaf191f87
Benoit DESROCHERS <ben.ensta@gmail.com>
2017-03-22 21:08:52 +0100

    [auto_build] fix PI constant un CtcAngle and build scripts

--- src/polar/ibex_CtcAngle.h
+++ src/polar/ibex_CtcAngle.h
@@ -56,14 +56,14 @@ protected:
 inline Interval Cmod(const Interval& x, const Interval& y){
   Interval xx(x);
   Interval yy(y);
-  bwd_imod(xx,yy,2*0x1.921fb54442d18p+1f);
+  bwd_imod(xx,yy,2*Interval::PI.ub());
   return yy;
 }
 
 inline Interval Cmod_bwd(const Interval& x, const Interval& y){
   Interval xx(x);
   Interval yy(y);
-  bwd_imod(xx,yy,2*0x1.921fb54442d18p+1f);
+  bwd_imod(xx,yy,2*Interval::PI.ub());
   return xx;
 }
 
